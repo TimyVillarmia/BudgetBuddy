eaf8ca2f3ae7a81089620a2bfb95db20fd08e692
ACT-STUDENT <ACT-STUDENT@LAB1-PC4>
2023-11-17 09:54:39 +0800

    added presenters

--- BudgetBuddy/Presenters/UserControls/SignIn.Designer.cs
+++ BudgetBuddy/Presenters/UserControls/SignIn.Designer.cs
@@ -28,10 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(320, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "SignIn";
+            this.Size = new System.Drawing.Size(625, 547);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
