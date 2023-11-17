ebd3078c7ed5fc48f6357bc58ea866b6690f898b
ACT-STUDENT <ACT-STUDENT@LAB1-PC4>
2023-11-17 09:25:13 +0800

    refactor: scaffold project

---
+++ BudgetBuddy/Models/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetBuddy.Models
+{
+    public class Account : IAccount
+    {
+        private int id;
+        private string name;
+        private string email;
+        private string password;
+
+        [DisplayName("Account Id")]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DisplayName("Fullname")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Name is required")]
+        public string Fullname
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DisplayName("Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email address is required")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Your Email is not valid.")]
+        public string Email { get { return email; } set { email = value; } }
+
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(8, ErrorMessage = "Password must be greater than 8")]
+        public string Password { get { return password; } set { password = value; } }
+    }
+}
+}
---
+++ BudgetBuddy/Models/IAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetBuddy.Models
+{
+    public interface IAccount
+    {
+        string Email { get; set; }
+        string Fullname { get; set; }
+        int Id { get; set; }
+        string Password { get; set; }
+    }
+}
